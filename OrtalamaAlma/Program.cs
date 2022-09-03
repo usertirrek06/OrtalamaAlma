1d9140656017a0a92279af1b6d420d2e13ac31a3
usertirrek06 <71853456+usertirrek06@users.noreply.github.com>
2022-09-04 02:26:16 +0300

    Add files via upload

---
+++ OrtalamaAlma/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OrtalamaAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        inputPoint:
+            Console.WriteLine("Please Enter a Number");
+            string input = Console.ReadLine();
+            int givenNumber = 0;
+            try
+            {
+                givenNumber = Convert.ToInt32(input);
+                if (givenNumber<0)
+                {
+                    goto inputPoint;
+                }
+                Console.WriteLine("Number : {0}",givenNumber);
+            }
+            catch (Exception)
+            {
+
+                goto inputPoint;
+            }
+            int result = ReturnFibonacci(givenNumber);
+            Console.WriteLine("Result : {0}",result);
+            int ReturnFibonacci(int givenNumber)
+            {
+                //if (givenNumber==0)
+                //{
+                //    return 0;
+                //}
+                //else if (givenNumber==1)
+                //{
+                //    return 1;
+                //}
+                //else
+                //{
+                //    return ReturnFibonacci(givenNumber - 1) + ReturnFibonacci(givenNumber - 2);
+                //}
+                int result;
+                result = givenNumber==0 ? 0 : givenNumber==1 ? 1 : ReturnFibonacci(givenNumber - 1) + ReturnFibonacci(givenNumber - 2);
+                return result;
+            }
+        }
+        
+    }
+}
