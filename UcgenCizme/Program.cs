26ee2470b067d606ced45be11d3c35524888652e
usertirrek06 <71853456+usertirrek06@users.noreply.github.com>
2022-09-04 02:44:56 +0300

    Add files via upload

---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        inputPoint:
+            Console.WriteLine("Please Enter a Positive Number");
+            string input = Console.ReadLine();
+            int givenNumber = 0;
+            try
+            {
+                givenNumber = Convert.ToInt32(input);
+                if (givenNumber == 0)
+                {
+                    goto inputPoint;
+                }
+            }
+            catch (Exception)
+            {
+
+                goto inputPoint;
+            }
+            DrawTriangle(givenNumber);
+
+            void DrawTriangle(int input) 
+            {
+                for (int i = 0; i < input; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            void DrawSquare(int input) 
+            {
+                for (int i = 0; i < input; i++)
+                {
+                    for (int j = 0; j < input; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
